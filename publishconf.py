47b6f59b3fa9a6cc38a1d92798a5318001bc291b
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2023-05-23 00:18:23 +0200

    Change site URL to https://static.transport-data.org

--- publishconf.py
+++ publishconf.py
@@ -8,7 +8,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *  # noqa: E402, F401, F403
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "https://www.transport-data.org"
+SITEURL = "https://static.transport-data.org"
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
